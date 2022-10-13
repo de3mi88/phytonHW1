954cea6cfaeb5c9c37ae413a1905abda62631630
de3mi88 <vladimirturbabin@gmail.com>
2022-10-13 20:55:22 +0300

    make hw

---
+++ lec.py
@@ -0,0 +1,47 @@
+#Задача 1
+#dayWeeks = int(input())
+
+#if dayWeeks==6 or dayWeeks==7:
+#   print('Выходной день')
+    
+#else:
+#    print('Будний день')
+
+#Задача 2
+#coordinatesX = int(input("Введите значение X "))
+
+#coordinatesY= int(input("Введите значение Y "))
+
+#if coordinatesX > 0 or coordinatesY > 0:
+#    print('2 секЦия')
+
+#elif coordinatesX > 0 or coordinatesY < 0:
+#    print('4 секЦия')
+
+#elif coordinatesX < 0 or coordinatesY < 0:
+#    print('3 секЦия')
+
+#elif coordinatesX < 0 or coordinatesY > 0:
+#    print('1 секЦия')
+
+#Задача 3
+#x_coordinate_A = float(input('Введите координату точки А по оси Х: '))
+#y_coordinate_A = float(input('Введите координату точки А по оси Y: '))
+#x_coordinate_B = float(input('Введите координату точки B по оси Х: '))
+#y_coordinate_B = float(input('Введите координату точки B по оси Y: '))
+
+#distance = round(math.sqrt((x_coordinate_B - x_coordinate_A)**2 + (y_coordinate_B - y_coordinate_A)**2), 2)
+#print(distance)
+
+#uarter = int(input('Введите номер четверти: '))
+
+#if quarter == 1:
+#   print('x > 0, y > 0')
+#elif quarter == 2:
+#   print('x < 0, y > 0')
+#elif quarter == 3:
+#    print('x < 0, y < 0')
+#elif quarter == 4:
+#   print('x > 0, y < 0')
+#else:
+#    print('На координатной плоскости только 4 четверти')
